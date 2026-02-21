f548256009c420f38b03a881f40bdae62805faed
tt <xiaotian3303@outlook.com>
2026-02-21 20:12:19 +0800

    Exercise- Using CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import ListGroup from "./components/ListGroup";
-import "./App.css";
-import { BsCalendarFill } from "react-icons/bs";
+import Button from "./components/Button/Button";
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   return (
     <div>
-      <BsCalendarFill color="red" size="40" />
-      <ListGroup
-        heading="Miami"
-        items={items}
-        onSelectItem={() => {}}
-      ></ListGroup>
+      <Button onClick={() => {}}>My Button</Button>
     </div>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import styles from "./Button.module.css";
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: Props) => {
+  console.log(styles);
+
+  return (
+    <button
+      className={[styles.btn, styles["btn-" + color]].join(" ")}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
