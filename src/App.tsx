713329c0cb4f3e9df02dbd71e78d2bd373cb04d7
tt <xiaotian3303@outlook.com>
2026-02-23 19:26:05 +0800

    building ExpenseFilter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "Groceries", amount: 50, category: "Food" },
     {
@@ -18,10 +20,19 @@ function App() {
     },
   ]);
 
+  const visivleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visivleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
