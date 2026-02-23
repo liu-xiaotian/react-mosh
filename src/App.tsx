4a80b685d0e279d55183bf25076ac96ec3f817e0
tt <xiaotian3303@outlook.com>
2026-02-23 19:10:14 +0800

    building expenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Groceries", amount: 50, category: "Food" },
+    {
+      id: 2,
+      description: "Electricity Bill",
+      amount: 100,
+      category: "Utilities",
+    },
+    {
+      id: 3,
+      description: "Movie Tickets",
+      amount: 30,
+      category: "Entertainment",
+    },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
