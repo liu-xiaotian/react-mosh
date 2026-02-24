f549bcf55bd2fb0f9e445e1a25ff2ddfba58a1e7
tt <xiaotian3303@outlook.com>
2026-02-24 17:47:02 +0800

    async and await

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import { useEffect } from "react";
+import axios, { AxiosError } from "axios";
+import { set } from "immer/dist/internal";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
+interface User {
+  id: number;
+  name: string;
+}
 
 const App = () => {
-  useEffect(() => {
-    connect();
-    return () => disconnect();
-  });
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState<string>("");
 
-  return <div>App</div>;
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/users",
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <ul>
+      {error && <p className="text-danger">{error}</p>}
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
