5068d2282f827ffd932e5bf6f8cc6b29880a890a
tt <xiaotian3303@outlook.com>
2026-02-24 17:15:01 +0800

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { useEffect, useRef, useState } from "react";
-import ProductList from "./components/ProductList";
+import { useEffect } from "react";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
 
 const App = () => {
-  const [category, setCategory] = useState("");
+  useEffect(() => {
+    connect();
+    return () => disconnect();
+  });
 
-  useEffect(() => {});
-
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
